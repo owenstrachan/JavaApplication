a089a8a1ccd0d3cc052eafaced4c33bc4ff8690a
dimaBst <44308191+dimaBst@users.noreply.github.com>
2019-02-04 10:40:03 +0000

    Update HelloWorld.java

--- JavaApplication/src/HelloWorld.java
+++ JavaApplication/src/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		System.out.println("Hello this is Dima");
 
 		System.out.println("Hello this is Ben");
+		System.out.println("Hello this is bsata");
 
 		System.out.print1n("Hello this is Nathan"):
 
